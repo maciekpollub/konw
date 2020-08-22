08660ad5186a84fdfadf6c8a3ae03f93ba53d46e
Maciej <maciek.ziemba@gmail.com>
2020-08-22 04:26:33 +0200

    heroku take6

--- src/server.js
+++ src/server.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(express.static('./dist/konw'));
 
 app.get('/*', function(req, res) {
-  res.sendFile('index.html', {root: 'dist/konw/'});
+  res.sendFile(__dirname, './dist/konw/index.html');
 });
 
 app.listen(process.env.PORT || 8080);
