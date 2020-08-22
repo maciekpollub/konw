8994d2320e18ee6f23911c26726fe09241aafa1b
Maciej <maciek.ziemba@gmail.com>
2020-08-22 03:42:05 +0200

    heroku take4

--- src/server.js
+++ src/server.js
@@ -2,10 +2,10 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static(__dirname + '/dist/konw'));
+app.use(express.static('./dist/konw'));
 
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/konw/index.html'));
+  res.sendFile('index.html', {root: 'dist/konw/'});
 });
 
-app.listen(process.env.PORT || 3000);
+app.listen(process.env.PORT || 8080);
