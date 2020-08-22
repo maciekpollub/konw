3f69198079b4ca284716698edbeb5794ad62588a
Maciej <maciek.ziemba@gmail.com>
2020-08-23 00:29:54 +0200

    heroku take10\

--- src/server.js
+++ src/server.js
@@ -2,9 +2,9 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static(__dirname + '/dist/konw'));
+app.use(express.static(path.join(__dirname,  './dist/konw')));
 
-app.get('/*', function(req, res) {
+app.get('/', function(req, res) {
   res.sendFile(__dirname, './dist/konw/index.html');
 });
 
