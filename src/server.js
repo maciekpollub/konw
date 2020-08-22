3f170335231157d53c9555ee443757a26a9d36c5
Maciej <maciek.ziemba@gmail.com>
2020-08-22 21:57:05 +0200

    heroku take7

--- src/server.js
+++ src/server.js
@@ -2,9 +2,9 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static('./dist/konw'));
+app.use(express.static(__dirname + '/dist/konw'));
 
-app.get('/*', function(req, res) {
+app.post('/*', function(req, res) {
   res.sendFile(__dirname, './dist/konw/index.html');
 });
 
